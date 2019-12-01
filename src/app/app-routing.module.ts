ee27e2fe25637f20e39c1e61d7d599da4df3df90
SarahMcGonigle <smcgonigle0@gmail.com>
2019-12-01 16:51:33 +0000

    Styling Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     component: ReadComponent
   },
   {
-    path:'',
+    path:'home-page',
     component: HomePageComponent
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,9 +40,12 @@ BrowserAnimationsModule,
 MatInputModule,
 MatDatepickerModule,
 MatNativeDateModule,
-FormsModule
+FormsModule,
+
 
   ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
